d13b2fcae6102097a1fe6975d385e8492d9f8fa3
timmythetiny <tim@timharding.co>
2015-09-01 14:29:06 -0700

    Added a blueprint .blend file and began work on main()

--- routes/api/v1/main.py
+++ routes/api/v1/main.py
@@ -1,7 +1,25 @@
-import bpy
-
 def main():
+    import bpy
+    initArgs()
     print('Hello world!')
+
+
+def initArgs():
+    pass
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--taskname')
+    args = parser.parse_args()
+    if args.taskname:
+        print('Taskname on')
+    else:
+        print('Taskname off, please try again')
+        sys.exit()
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
