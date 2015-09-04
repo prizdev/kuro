de7cb1491c4cc90ca1dfe87fcadfb3a4d162a0e1
timmythetiny <tim@timharding.co>
2015-09-04 10:02:19 -0700

    Blender script working
    takes -t taskname (######) -nogl nogl render resolution -gif gif render
    resolution

--- routes/api/v1/main.py
+++ routes/api/v1/main.py
@@ -1,23 +1,83 @@
+# Blender Server 3.0 - Project Kuro
+
+import bpy
+import os
+import sys
+import mathutils
+import math
+
+
 def main():
-    import bpy
-    initArgs()
-    print('Hello world!')
+    filesIn = initInputs(0)
+    resNogl = initInputs(1)
+    resGif  = initInputs(2)
+    importAssets(filesIn)
+    materialAssignment(filesIn)
+    renderScene(resNogl, False)
+    renderScene(resGif, True)
+    processOutput(filesIn)
+    print('Finished script!')
 
 
-def initArgs():
-    pass
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--taskname')
-    args = parser.parse_args()
-    if args.taskname:
-        print('Taskname on')
+def initInputs(outputSwitch):
+    taskname = str(sys.argv[sys.argv.index('-t') + 1])
+    resNogl  = int(sys.argv[sys.argv.index('-nogl') + 1])
+    resGif   = int(sys.argv[sys.argv.index('-gif') + 1])
+    assetsFolder = os.path.join('E:\\', 'Prizmiq', 'Misc', 'Dev', 'Github', 'kuro', 'assets', 'testFilepath', taskname, 'input')
+    fileNames    = ['geometry.obj', 'diffuse.jpg', 'specular.jpg', 'normal.jpg']
+    filesIn      = []
+    for name in fileNames:
+        filesIn.append(assetsFolder + '\\' + name)
+    if outputSwitch == 0:
+        return(filesIn)
+    elif outputSwitch == 1:
+        return(resNogl)
     else:
-        print('Taskname off, please try again')
-        sys.exit()
+        return(resGif)
 
 
+def importAssets(filesIn):
+    #blueprint blend import
+    bpy.ops.wm.open_mainfile(filepath = 'E:\\Prizmiq\\Misc\\Dev\\Github\\kuro\\assets\\blueprintBlend\\blueprint_v001_003.blend')
+    #obj import
+    bpy.ops.import_scene.obj(filepath = filesIn[0])
 
+
+def materialAssignment(filesIn):
+    for obj in bpy.data.objects:
+        if 'Group' in obj.name:
+            obj.data.materials[0] = bpy.data.materials['Material']
+    #print(bpy.data.materials['Material'].node_tree.nodes[5]
+
+    mat                = bpy.data.materials['Material']
+    nodes              = mat.node_tree.nodes
+    node_texture       = bpy.data.materials['Material'].node_tree.nodes[5]
+    emission_shader    = bpy.data.materials['Material'].node_tree.nodes[2]
+    node_texture.image = bpy.data.images.load(filesIn[1])
+    links              = mat.node_tree.links
+    link               = links.new(node_texture.outputs[0], emission_shader.inputs[0])
+
+
+def renderScene(resolution, gifBool):
+    if gifBool == True:
+        bpy.data.scenes['Scene'].frame_start = 82
+        bpy.data.scenes['Scene'].frame_end   = 601
+    else:
+        bpy.data.scenes['Scene'].frame_start = 1
+        bpy.data.scenes['Scene'].frame_end   = 81
+
+    render = bpy.data.scenes['Scene'].render
+    render.resolution_x = resolution * 2
+    render.resolution_y = resolution * 2
+    #for axis in [render.resolution_x, render.resolution_y]:
+    #    axis = resolution
+
+    render.filepath = 'E:\\Prizmiq\\Misc\\Dev\\Github\\kuro\\assets\\renderFilepath\\render.jpg'
+    bpy.ops.render.render(animation = True, write_still = True)
+
+
+def processOutput(filesIn):
+    pass
 
 
 
